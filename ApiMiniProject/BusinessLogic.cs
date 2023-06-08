1e15039d97955039f2101a548675b5ddfcce1065
timgriffin-io <tim-griffin@outlook.com>
2023-06-08 05:48:21 -0500

    Add project files.

---
+++ ApiMiniProject/BusinessLogic.cs
@@ -0,0 +1,28 @@
+﻿using ApiMiniProject.Models;
+
+namespace ApiMiniProject;S
+
+public static class BusinessLogic
+{
+    public static bool PersonIsValid(PersonModel person, List<PersonModel> persons)
+    {
+        bool isValid = true;
+        if (person.Id < 0)
+        {
+            isValid = false;
+        }
+        if (persons.Any(existingPerson => existingPerson.Id == person.Id))
+        {
+            isValid = false;
+        }
+        if (string.IsNullOrEmpty(person.FirstName))
+        {
+            isValid = false;
+        }
+        if (string.IsNullOrEmpty(person.LastName))
+        {
+            isValid = false;
+        }
+        return isValid;
+    }
+}
---
+++ ApiMiniProject/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using ApiMiniProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ApiMiniProject.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PersonController : ControllerBase
+{
+    private readonly ILogger<PersonController> _logger;
+    private static List<PersonModel> _persons = new();
+    public PersonController(ILogger<PersonController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("GetPeople")]
+    public IActionResult GetPeople()
+    {
+        return Ok(_persons);
+    }
+
+    [HttpGet("GetPerson/{id}")]
+    public IActionResult GetPerson(int id)
+    {
+        return Ok(_persons.Where(x => x.Id == id));
+    }
+
+
+    [HttpPost]
+    public IActionResult Post([FromBody] PersonModel person)
+    {
+        if (BusinessLogic.PersonIsValid(person, _persons))
+        {
+            _persons.Add(person);
+            _logger.LogInformation($"{person.Id} : {person.FirstName} {person.LastName} was added to persons list");
+            return Ok();
+        }
+
+        _logger.LogInformation("Invalid Person Object");
+        return BadRequest();
+
+    }
+}
